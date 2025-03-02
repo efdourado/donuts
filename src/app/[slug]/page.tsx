d0dcf50a5499a145f4f7021eb994db9c8a4f65ec
efdourado <ed320819@gmail.com>
2025-03-02 13:18:18 -0300

    Code cleanup and minor fixes

--- src/app/[slug]/page.tsx
+++ src/app/[slug]/page.tsx
@@ -4,11 +4,11 @@ import { db } from "@/lib/prisma";
 import OrderTypeOption from "./components/orderTypeOption";
 
 interface RestaurantPageProps {
-  params: { slug: string };
+  params: Promise<{ slug: string }>;
 }
 
 const RestaurantPage = async ({ params }: RestaurantPageProps) => {
-  const { slug } = params;
+  const { slug } = await params;
   const restaurant = await db.restaurant.findUnique({ where: { slug } });
   if (!restaurant) {
     return notFound();
