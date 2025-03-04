133864a3eb39eefe3596ea35c43ea6ce2d9bd8df
efdourado <ed320819@gmail.com>
2025-03-04 11:10:34 -0300

    Code cleanup and minor fixes

--- src/app/[slug]/menu/components/categories.tsx
+++ src/app/[slug]/menu/components/categories.tsx
@@ -75,7 +75,9 @@ const RestaurantCategories = ({ restaurant }: RestaurantCategoriesProps) => {
 
       <h3 className="px-5 pt-2 font-semibold">{selectedCategory.name}</h3>
       <Products products={selectedCategory.products} />
+
       <div className="pb-20"></div>
+      
       {products.length > 0 && (
         <div className="fixed bottom-0 left-0 right-0 flex w-full products-center justify-between border-t bg-white px-5 py-3">
           <div>
