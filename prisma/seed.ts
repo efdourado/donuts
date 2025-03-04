65b854e686eee5ceddc9ffb1858f6a4c5508fce2
efdourado <ed320819@gmail.com>
2025-03-04 10:03:58 -0300

    Code cleanup and minor fixes

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -14,6 +14,28 @@ const main = async () => {
         bannerUrl: "/mcdonaldsbanner.jpg",
     }, });
 
+    const breakfastCategory = await tx.category.create({
+      data: {
+        name: "Breakfast",
+        restaurantId: restaurant.id,
+    }, });
+    await tx.product.createMany({
+      data: [
+        {
+          name: "Hotcakes",
+          description: "If you love hot pancakes, you've got to try McDonald's Hotcakes with a side of real butter and sweet maple flavored Hotcake syrup.",
+          ingredients: [
+            "3 Hotcakes;",
+            "Hotcake Syrup;",
+            "Salted Whipped Butter.",
+          ],
+          price: 6.5,
+          imageUrl:"https://corporate.mcdonalds.com/content/dam/sites/corp/nfl/newsroom/menu-items-2023/NR_202208_0031_3HotCakes_2000x2000.png.coredownload.png",
+          categoryId: breakfastCategory.id,
+          restaurantId: restaurant.id,
+  }, ], });
+
+
     const dealsCategory = await tx.category.create({
       data: {
         name: "Deals",
@@ -88,7 +110,7 @@ const main = async () => {
           name: "Fries",
           description: "Thin deep-fried potato stripes are one of the most popular fast-food items, which are loved by absolutely everyone. The main advantage of this dish is that it goes perfectly with any sauce, so you can experiment with its taste!",
           ingredients: [],
-          price: 10.9,
+          price: 2.9,
           imageUrl: "https://corporate.mcdonalds.com/content/dam/sites/corp/nfl/newsroom/8932_MediumFries.uuid.png.coredownload.png",
           categoryId: sidesCategory.id,
           restaurantId: restaurant.id,
@@ -97,7 +119,7 @@ const main = async () => {
           name: "4 pc. Chicken McNuggets®",
           description: "Mechanically separated chicken with an appetizing thick coating will appeal to everyone, who wants to try a chicken dish from McDonald’s",
           ingredients: [],
-          price: 6.9,
+          price: 3.9,
           imageUrl: "https://corporate.mcdonalds.com/content/dam/sites/corp/nfl/newsroom/menu-items-2023/NR_202006_0483_4McNuggets_Stacked_2000x2000.png.coredownload.png",
           categoryId: sidesCategory.id,
           restaurantId: restaurant.id,
@@ -106,7 +128,7 @@ const main = async () => {
           name: "Apple Slices",
           description: "McDonald's Apple Slices are a wholesome, tasty side made from real apples. Specially selected varieties mean our apple slices are always crisp and juicy, making for a tasty snack with 15 calories per labelled serving. Enjoy it as a Snack or Side to your meal!",
           ingredients: [],
-          price: 4.9,
+          price: 1.5,
           imageUrl: "https://corporate.mcdonalds.com/content/dam/sites/corp/nfl/newsroom/menu-items-2023/NR_202002_2794_AppleSlices_NoBag_2000x2000.png.coredownload.png",
           categoryId: sidesCategory.id,
           restaurantId: restaurant.id,
@@ -114,14 +136,14 @@ const main = async () => {
 
     const beveragesCategory = await tx.category.create({
       data: {
-        name: "Beverages",
+        name: "Beverages & Milkshakes",
         restaurantId: restaurant.id,
     }, });
     await tx.product.createMany({
       data: [
         {
           name: "Coke®",
-          description: "Enjoy your favourite McDonald's meal with a Coke. The original, refreshing ice-cold cola that goes well with your meal and completes the experience.",
+          description: "The original, refreshing ice-cold cola that goes well with your meal and completes the experience.",
           ingredients: [],
           price: 6.9,
           imageUrl: "https://corporate.mcdonalds.com/content/dam/sites/corp/nfl/newsroom/menu-items-2023/NR_202112_0521_MediumCoke_Glass_2000x2000.png.coredownload.png",
@@ -133,6 +155,19 @@ const main = async () => {
           description: "The great taste of crisp, clear, Sprite. Made with 100% natural lemon-lime flavours, with no colours and no caffeine.",
           ingredients: [],
           price: 8.9,
+          imageUrl: "https://corporate.mcdonalds.com/content/dam/sites/corp/nfl/newsroom/menu-items-2023/NR_202212_0721_MediumSprite_Glass_2000x2000.png.coredownload.png",
+          categoryId: beveragesCategory.id,
+          restaurantId: restaurant.id,
+        },
+        {
+          name: "Chocolate Shake",
+          description: "Our chocolate shake recipe features delicious soft serve and chocolate syrup, finished with whipped light cream.",
+          ingredients: [
+            "Vanilla Reduced Fat Ice Cream;",
+            "Chocolate Shake Syrup;",
+            "Whipped Light Cream.",
+          ],
+          price: 9.9,
           imageUrl: "https://corporate.mcdonalds.com/content/dam/sites/corp/nfl/newsroom/menu-items-2023/NR_202212_0721_MediumSprite_Glass_2000x2000.png.coredownload.png",
           categoryId: beveragesCategory.id,
           restaurantId: restaurant.id,
@@ -173,9 +208,23 @@ const main = async () => {
     await tx.product.createMany({
       data: [
         {
+          name: "Americano",
+          description: "Our simple yet satisfying Americano is made with hot water poured over our Rainforest Alliance Certified™ espresso.",
+          ingredients: [],
+          price: 2.9,
+          imageUrl: "https://corporate.mcdonalds.com/content/dam/sites/corp/nfl/newsroom/menu-items-2023/NR_201906_1318_MediumAmericano_Glass_A1_HL_2000x2000.png.coredownload.png",
+          categoryId: coffeesCategory.id,
+          restaurantId: restaurant.id,
+        },
+        {
           name: "Iced Coffee",
           description: "The McCafé Iced Coffee features 100% Arabica beans, cream and your choice of flavor like Caramel, French Vanilla and Sugar-Free French Vanilla.* A fan of black coffee? Get it without the sugar or cream to refresh your mornings with a cool Black Iced Coffee.",
-          ingredients: [],
+          ingredients: [
+            "Brewed Coffee;",
+            "Ice;",
+            "Light Cream;",
+            "Liquid Sugar.",
+          ],
           price: 23.9,
           imageUrl: "https://corporate.mcdonalds.com/content/dam/sites/corp/nfl/newsroom/menu-items-2023/NR_201906_1212_MediumIcedCoffee_Glass_A1_2000x2000.png.coredownload.png",
           categoryId: coffeesCategory.id,
@@ -184,12 +233,101 @@ const main = async () => {
         {
           name: "Iced French Vanilla Latte",
           description: "Cool down with a McCafé® Iced French Vanilla Latte, made with Rainforest Alliance Certified™ espresso. It features bold espresso, whole milk and sweet French Vanilla syrup.",
-          ingredients: [],
-          price: 23.9,
+          ingredients: [
+            "Whole Milk;",
+            "Ice;",
+            "French Vanilla Syrup",
+            "Water;",
+            "Espresso.",
+          ],
+          price: 21.6,
           imageUrl: "https://corporate.mcdonalds.com/content/dam/sites/corp/nfl/newsroom/menu-items-2023/NR_201906_0187_MediumIcedFrenchVanillaLatte_Glass_A1_2000x2000.png.coredownload.png",
           categoryId: coffeesCategory.id,
           restaurantId: restaurant.id,
-},] });});};
+        },
+        {
+          name: "Iced Caramel Macchiato",
+          description: "The McDonald's Iced Caramel Macchiato recipe features bold, rich and dark-roasted espresso and buttery caramel syrup with extra caramel drizzle. Served with whole milk, it's almost similar to an Iced Caramel Latte but with an extra buttery caramel drizzle.",
+          ingredients: [
+            "Whole Milk;",
+            "Ice;",
+            "Caramel Syrup;",
+            "Water;",
+            "Espresso;",
+            "Caramel Drizzle.",
+          ],
+          price: 16.9,
+          imageUrl: "https://corporate.mcdonalds.com/content/dam/sites/corp/nfl/newsroom/menu-items-2023/NR_201906_2743_MediumIcedCaramelMacchiato_Glass_A1_2000x2000.png.coredownload.png",
+          categoryId: coffeesCategory.id,
+          restaurantId: restaurant.id,
+        },
+        {
+          name: "Caramel Frappé",
+          description: "The McCafé Caramel Frappé recipe blends rich caramel flavor with a hint of coffee, blended with ice and is topped with whipped light cream.",
+          ingredients: [
+            "Ice;",
+            "Caramel Coffee Frappe Base",
+            "Whipped Light Cream",
+            "Caramel Drizzle.",
+          ],
+          price: 26.9,
+          imageUrl: "https://corporate.mcdonalds.com/content/dam/sites/corp/nfl/newsroom/menu-items-2023/NR_201906_2842_MediumCaramelFrappe_Glass_A1_2000x2000.png.coredownload.png",
+          categoryId: coffeesCategory.id,
+          restaurantId: restaurant.id,
+    }, ] });
+
+    const sweetsCategory = await tx.category.create({
+      data: {
+        name: "Sweets & Treats",
+        restaurantId: restaurant.id,
+    }, });
+    await tx.product.createMany({
+      data: [
+        {
+          name: "Chocolate Cookies",
+          description: "An amazingly delicious, soft and chewy Chocolate Cookie. Our recipe features a perfectly warm, soft baked cookie loaded with gooey chocolate chips. Enjoy it on its own as a snack or pair it with your favorite McDonald's meal.",
+          ingredients: [],
+          price: 1.6,
+          imageUrl: "https://corporate.mcdonalds.com/content/dam/sites/corp/nfl/newsroom/menu-items-2023/NR_202004_1852_ChocolateChipCookie_Broken_2000x2000.png.coredownload.png",
+          categoryId: sweetsCategory.id,
+          restaurantId: restaurant.id,
+        },
+        {
+          name: "Baked Apple Pie",
+          description: "McDonald's Baked Apple Pie recipe features 100% American-grown apples, and a lattice crust baked to perfection and topped with sprinkled sugar.",
+          ingredients: [],
+          price: 8.2,
+          imageUrl: "https://corporate.mcdonalds.com/content/dam/sites/corp/nfl/newsroom/menu-items-2023/NR_202004_0706_BakedApplePie_Broken_2000x2000.png.coredownload.png",
+          categoryId: sweetsCategory.id,
+          restaurantId: restaurant.id,
+        },
+        {
+          name: "Vanilla Cone",
+          description: "Our Vanilla Cone features creamy vanilla soft serve in a crispy cone. It's the perfect sweet treat in addition to any McDonald's meal or on its own.",
+          ingredients: [],
+          price: 3.8,
+          imageUrl: "https://corporate.mcdonalds.com/content/dam/sites/corp/nfl/newsroom/0366_VanillaCone.uuid.png.coredownload.png",
+          categoryId: sweetsCategory.id,
+          restaurantId: restaurant.id,
+        },
+        {
+          name: "Caramel Sundae",
+          description: "This Caramel Sundae combines creamy vanilla soft serve and warm, buttery caramel topping.",
+          ingredients: [],
+          price: 8.6,
+          imageUrl: "https://corporate.mcdonalds.com/content/dam/sites/corp/nfl/newsroom/0345_CaramelSundae.uuid.png.coredownload.png",
+          categoryId: sweetsCategory.id,
+          restaurantId: restaurant.id,
+        },
+        {
+          name: "McFlurry® with OREO® Cookies",
+          description: "The McDonald's McFlurry® with OREO® Cookies is a popular combination of creamy vanilla soft serve with crunchy pieces of OREO® cookies! ",
+          ingredients: [],
+          price: 12.8,
+          imageUrl: "https://corporate.mcdonalds.com/content/dam/sites/corp/nfl/newsroom/0345_CaramelSundae.uuid.png.coredownload.png",
+          categoryId: sweetsCategory.id,
+          restaurantId: restaurant.id,
+}, ] }); });};
 
 main()
   .catch((e) => { throw e; })
